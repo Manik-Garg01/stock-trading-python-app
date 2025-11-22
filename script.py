0161b614ac6cf287652efa7e1802c9b9e247807b
Manik Garg <manik.garg@theporter.in>
2025-11-22 08:29:01 +0530

    Updated the script.py, added requirements.txt and also added the .gitignore and .env in the changes

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
-API_KEY = "B_3WpSYrAqZMHMJposMwgotc_956f8Or"
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-print('Hello World')
+POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
+
+url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&apiKey={POLYGON_API_KEY}"
+response = requests.get(url)
+print(response.json())
